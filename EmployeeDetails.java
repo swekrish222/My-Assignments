e45f8f03788d4410cf63a738a91527ced9711ea0
swekrish222 <swekrish222@gmail.com>
2025-12-07 13:51:30 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Day1Assignment;
+
+/**
+ * 
+ */
+public class EmployeeDetails {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String EmployeeName="Richard Davidson";
+		short Age=22;
+		int MonthlySalary=50000;
+		long Mobile=9821345671l;
+		int Pincode=600091;
+		float RatingAvg=99.2345f;
+		boolean Relocate=true;
+		String City="Hyderabad";
+		char profiencyassesment='N';
+		System.out.println("Employee Name="+EmployeeName+"\n"+"Employee Age="+Age+"\n"+"Salary="+MonthlySalary+"\n"+"Mobile Number="+Mobile+"Pincode="+Pincode+"\n"+"Average Rating="+RatingAvg+"\n"+"Willing to Relocate="+Relocate+"\n"+"Currently Residing in="+City+"\n"+"Completed assesment="+profiencyassesment+"\n");
+				
+
+	}
+
+}
